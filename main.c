0b41dcd9b40c8b35551c17a97a168805368bd30f
Luis Menina <liberforce@freeside.fr>
2012-03-08 01:20:27 +0100

    Add support for '1d+3'-style arguments.

--- main.c
+++ main.c
@@ -26,24 +26,37 @@ void print_roll (Roll *roll)
 
 int main (int argc, char **argv)
 {
-	int n_dice;
-	
 	if (argc != 2)
 	{
 		printf ("Usage: dice-prob N_DICE\n");
 		return -1;
 	}
 
-	n_dice = atoi (argv[1]);
+	char *str_n_dice = argv[1];
+	char *ptr = argv[1];
+
+	do
+	{
+		if  (*ptr == 'd' || *ptr == 'D')
+		{
+			*ptr = '\0';
+		}
+	}
+	while (*ptr++);
+
+	char *str_modifier = ptr;
+	int n_dice = atoi (str_n_dice);
+	int modifier = atoi (str_modifier);
 
 	Roll *roll = roll_new (n_dice);
-	int counter = 0;
-	int i;
-	int maxval = pow (6, n_dice);
+	roll_set_modifier (roll, modifier);
 
-	for (i = 0; i < maxval ; i ++)
+	int maxval = pow (6, n_dice);
+	int i;
+
+	for (i = 0; i < maxval ; i++)
 	{
-		roll_set_id (roll, counter++);
+		roll_set_id (roll, i);
 		print_roll (roll);
 	}
 
